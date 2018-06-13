25f87c74e4986b836f5323a1323c8d7168264c2c
YouDKC <1453137592@qq.com>
2018-06-13 22:58:36 +0800

    下班喽

--- first/quick_sort2.py
+++ first/quick_sort2.py
@@ -1,14 +1,20 @@
+
+lst = [49,38,65,97,76,13,27,49] 
+
 def quick_sort(lst):
 	def qsort(lst,begin,end):
 		if begin >= end:
 			return
-		pivot = lst[begin].key
+		pivot = lst[begin]
 		i = begin
 		for j in range(begin + 1,end + 1):
-			if lst[j].key < pivot:
+			if lst[j] < pivot:
 				i += 1
 				lst[i],lst[j] = lst[j],lst[i]
+#第一个循环，i=0,j=1,,
 		lst[begin],lst[i] = lst[i],lst[begin]
 		qsort(lst,begin,i-1)
 		qsort(lst,i+1,end)
 	qsort(lst,0,len(lst)-1)
+
+quick_sort(lst)
