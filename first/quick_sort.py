3d0b579bab1015e5c5fcb960a0be3ffa4427c585
YouDKC <1453137592@qq.com>
2018-06-13 20:56:23 +0800

    快速排序算法

---
+++ first/quick_sort.py
@@ -0,0 +1,22 @@
+
+lists  = [49,38,65,97,76,13,27,49]  
+def quick_sort(lists, left, right):  
+    # 快速排序  
+    if left >= right:  
+        return lists  
+    key = lists[left]  
+    low = left  
+    high = right  
+    while left < right:  
+        while left < right and lists[right] >= key:  
+            right -= 1  
+        lists[left] = lists[right]  
+        while left < right and lists[left] <= key:  
+            left += 1  
+        lists[right] = lists[left]  
+    lists[right] = key  
+    quick_sort(lists, low, left - 1)  
+    quick_sort(lists, left + 1, high)  
+    return lists  
+
+print(quick_sort())
