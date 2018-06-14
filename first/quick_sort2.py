7b1c9319c6108d27c69d10406aa4d6030e948bc9
YouDKC <1453137592@qq.com>
2018-06-14 12:19:38 +0800

    猎户星空面试准备

--- first/quick_sort2.py
+++ first/quick_sort2.py
@@ -11,8 +11,9 @@ def quick_sort(lst):
 			if lst[j] < pivot:
 				i += 1
 				lst[i],lst[j] = lst[j],lst[i]
-#第一个循环，i=0,j=1,,
+#第一个循环，i=0,j=1,i的作用就是记录小于pivot的元素的个数，交换两个元素
 		lst[begin],lst[i] = lst[i],lst[begin]
+#将pivot放在应在的位置,至此一个qsort基本结束
 		qsort(lst,begin,i-1)
 		qsort(lst,i+1,end)
 	qsort(lst,0,len(lst)-1)
